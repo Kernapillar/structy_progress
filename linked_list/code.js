1080b05d00cafd9546f1c5582285d96b984dd7b5
Alexander Kern <alexander.t.kern@gmail.com>
2022-08-29 16:40:45 -0700

    merge lists

--- linked_list/code.js
+++ linked_list/code.js
@@ -74,3 +74,35 @@ const zipperLists = (head1, head2) => {
     }
     return retHead;
 };
+
+const mergeLists = (head1, head2) => {
+  // todo
+  const preNode = new Node;
+  let current = preNode;
+  while (head1 || head2) {
+    if (head2 && head1){
+        if (head1.val < head2.val) {
+        current.next = head1;
+        current = current.next;
+        head1 = head1.next;
+      } else {
+        current.next = head2;
+        current = current.next
+        head2 = head2.next
+      } 
+    } else {
+        if (!head2) {
+          current.next = head1;
+          current = current.next;
+          head1 = head1.next;
+        } else {
+          current.next = head2;
+          current = current.next;
+          head2 = head2.next;
+        }
+      }
+    }
+    
+  
+  return preNode.next
+};
