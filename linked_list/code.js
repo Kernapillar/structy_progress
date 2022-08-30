cb9fd3b72ddbe8d8c6219fec4b4858b81a3d72af
Alexander Kern <alexander.t.kern@gmail.com>
2022-08-30 16:27:08 -0700

    create Linked List

--- linked_list/code.js
+++ linked_list/code.js
@@ -187,3 +187,18 @@ const insertNode = (head, value, index) => {
   }
   return head
 };
+
+const createLinkedList = (values) => {
+  // todo
+  if (values.length === 0) {
+    return null
+  }
+  const head = new Node(values[0]);
+  let current = head;
+  for (let i = 1; i< values.length; i++) {
+    let newNode = new Node(values[i]);
+    current.next = newNode;
+    current = current.next
+  }
+  return head
+};
