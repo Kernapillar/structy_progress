e3f44f47d507dd2fd92f070367aa130f31d276f9
Alexander Kern <alexander.t.kern@gmail.com>
2022-10-28 13:41:32 -0700

    sum-possible

--- dynamic_Programming/code.js
+++ dynamic_Programming/code.js
@@ -22,3 +22,21 @@ const tribonacci = (n, memory = {}) => {
     memory[n] = current;
     return current
 };
+
+
+
+const sumPossible = (amount, numbers, memo = {}) => {
+    // todo
+    if (amount === 0) return true;
+    if (amount <0 ) return false;
+    if (amount in memo ) return memo[amount];
+    for (let num of numbers) {
+      if (sumPossible(amount - num, numbers, memo)) {
+        memo[amount] = true
+        return true;
+      }
+    }
+    
+    memo[amount] = false
+    return false;
+};
